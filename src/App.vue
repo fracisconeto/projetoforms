c26e71c121a2cd303efc3119f233c1c9219fa226
fracisconeto <vargasfrancisco227@gmail.com>
2024-07-05 15:03:43 -0300

    pronto

--- src/App.vue
+++ src/App.vue
@@ -1,100 +1,272 @@
 <script setup>
-import { reactive, ref } from 'vue'
+import { ref, reactive } from 'vue'
 
-const titulo = ref('ola seus putos')
+const estados = [
+  { sigla: 'AC', name: 'Acre' },
+  { sigla: 'AL', name: 'Alagoas' },
+  { sigla: 'AP', name: 'Amapá' },
+  { sigla: 'AM', name: 'Amazonas' },
+  { sigla: 'BA', name: 'Bahia' },
+  { sigla: 'CE', name: 'Ceará' },
+  { sigla: 'DF', name: 'Distrito Federal' },
+  { sigla: 'ES', name: 'Espírito Santo' },
+  { sigla: 'GO', name: 'Goiás' },
+  { sigla: 'MA', name: 'Maranhão' },
+  { sigla: 'MT', name: 'Mato Grosso' },
+  { sigla: 'MS', name: 'Mato Grosso do Sul' },
+  { sigla: 'MG', name: 'Minas Gerais' },
+  { sigla: 'PA', name: 'Pará' },
+  { sigla: 'PB', name: 'Paraíba' },
+  { sigla: 'PR', name: 'Paraná' },
+  { sigla: 'PE', name: 'Pernambuco' },
+  { sigla: 'PI', name: 'Piauí' },
+  { sigla: 'RJ', name: 'Rio de Janeiro' },
+  { sigla: 'RN', name: 'Rio Grande do Norte' },
+  { sigla: 'RS', name: 'Rio Grande do Sul' },
+  { sigla: 'RO', name: 'Rondônia' },
+  { sigla: 'RR', name: 'Roraima' },
+  { sigla: 'SC', name: 'Santa Catarina' },
+  { sigla: 'SP', name: 'São Paulo' },
+  { sigla: 'SE', name: 'Sergipe' },
+  { sigla: 'TO', name: 'Tocantins' }
+]
 
-const produto = reactive({
+const hobbies = reactive([
+  { id: 1, hobbie: 'Esporte' },
+  { id: 2, hobbie: 'Ler' },
+  { id: 3, hobbie: 'Cozinhar' },
+  { id: 4, hobbie: 'Viajar' }
+])
+
+const linguagens = reactive([
+  { id: 1, tipo: 'Java' },
+  { id: 2, tipo: 'C++' },
+  { id: 3, tipo: 'Python' },
+  { id: 4, tipo: 'PHP' }
+])
+
+const pessoa = reactive({
   nome: '',
+  idade: 18,
   email: '',
   senha: '',
-  categorias: []
+  nascimento: '',
+  estado: '',
+  cidade: '',
+  endereco: '',
+  hobbies: [],
+  linguagens: [],
+  biografia: ''
 })
 
-   
+const show = ref(false)
+const senhaVerificada = ref('')
 
-const mostrarResultado = false
+function EnviarDados() {
+  if (senhaVerificada.value == pessoa.senha) {
+    show.value = true
+  } else {
+    alert('As senhas não correspondem corretamente!')
+    document.getElementById('senhaConfirma').focus()
+  }
+}
 </script>
 
+
+
 <template>
-  <div class="putos"> /h1>
-  </div>
-  <div class="container">
-    <div class="forms">
-      <h2>formulários</h2>
-      <input type="text" v-model="titulo" />
-      <div>
-        <div class="row">
-          <label for="nome">nome:</label>
-          <input type="text" v-model="produto.nome" />
+  <div class="custom-form">
+    <h1>Formulário Interligado </h1>
+    <div class="usuario" v-if="show">
+      <p v-for="(dado, key) of pessoa" :key="key">{{ key }}: {{ dado }}</p>
+      <button @click="show = false" class="custom-submit">Voltar</button>
+    </div>
 
-          <label for="email">email:</label>
-          <input type="email" v-model="produto.email">
+    <form v-else @submit.prevent="EnviarDados">
+      <div class="custom-input-container">
+        <label for="nome">Digite seu nome:</label>
+        <input
+          type="text"
+          v-model="pessoa.nome"
+          minlength="3"
+          maxlength="20"
+          autocomplete="on"
+          required
+        />
+      </div>
 
-          <label for="senha">senha:</label>
-          <input type="password" v-model="produto.senha">
-
-          <label for="confirma">confirmação de senha:</label>
-          <input type="password" v-model="produto.confirma">
-
-          <label for="data">data de nacimento:</label>
-          <input type="date" v-model="produto.data">
-              
-          <label for="cidade">cidade:</label>
-          <input type="text" v-model="produto.cidade">
-        
-          <label for="estado">estado:</label>
-          <input type="select" >
-        
+      <div class="custom-input-container">
+        <label for="email">Digite seu email:</label>
+         <input v-model="pessoa.email" placeholder="Por favor, insira um email válido!" type="email" required>
+      </div>
+      <div class="custom-input-container">
+        <label for="senha">Digite sua senha:</label>
+        <input type="password" v-model="pessoa.senha" minlength="8" required />
+      </div>
+      <div class="custom-input-container">
+        <label for="senhaConfirma">Confirme sua senha:</label>
+        <input
+          type="password"
+          id="senhaConfirma"
+          v-model="senhaVerificada"
+          minlength="8"
+          required
+        />
+      </div>
+      <div class="custom-input-container">
+        <label for="date">Data de nascimento:</label>
+         <input v-model="pessoa.nascimento" type="date" required>
+      </div>
+    <div class="custom-input-container">
+        <label for="idade">Digite sua idade:</label>
+        <input type="number" v-model="pessoa.idade" min="18" max="60" required />
+      </div>
+      <div class="custom-input-container">
+        <label for="estado">Informe seu estado:</label>
+        <select v-model="pessoa.estado">
+          <option v-for="estado in estados" :key="estado.sigla" :value="estado.sigla">
+            {{ estado.name }}
+          </option>
+        </select>
+      </div>
+      <div class="custom-input-container">
+        <label for="cidade">Informe sua cidade:</label>
+        <input type="text" v-model="pessoa.cidade" />
+      </div>
+      <div class="custom-input-container">
+        <label for="endereco">Informe seu endereço:</label>
+        <input type="text" v-model="pessoa.endereco" />
+      </div>
+      <div class="custom-check-container">
+        <label>Selecione seus hobbies:</label>
+        <div class="custom-checkbox" v-for="passatempo in hobbies" :key="passatempo.id">
+          <input
+            type="checkbox"
+            :value="passatempo.hobbie"
+            v-model="pessoa.hobbies"
+            :id="passatempo.hobbie"
+          />
+          <label :for="passatempo.hobbie">{{ passatempo.hobbie }}</label>
         </div>
       </div>
-    </div>
-
-    <div class="resultado">
-      <h2>dados do produto</h2>
-      <p>nome: {{ produto.nome }}</p>
-      <p>email:{{ produto.email }}</p>
-      <p>senha:{{ produto.senha }}</p>
-      <p>confirmação da senha:{{ produto.confirma }} </p>
-      <p>data de nacimento:{{ produto.data }}</p>
-      <p>cidade:{{produto.cidade}}</p>
-    </div>
+      <div class="custom-check-container">
+        <label>Selecione as linguagens que domina:</label>
+        <div class="custom-checkbox" v-for="linguagem in linguagens" :key="linguagem.id">
+          <input
+            type="checkbox"
+            :value="linguagem.tipo"
+            v-model="pessoa.linguagens"
+            :id="linguagem.tipo"
+          />
+          <label :for="linguagem.tipo">{{ linguagem.tipo }}</label>
+        </div>
+      </div>  
+      <div class="custom-bio-container">
+        <label for="biografia">Faça uma breve autobiografia:</label>
+        <textarea v-model="pessoa.biografia"></textarea>
+      </div>
+      <div class="custom-submit-container">
+        <input type="submit" value="Enviar dados" class="custom-submit" />
+      </div>
+    </form>
   </div>
 </template>
 
-<style scoped>
-.putos {
+<style>
+
+
+.custom-form {
+  max-width: 40%;
+  margin: 0px auto 0px auto;
+  padding: 20px;
+  border-radius: 10px;
+  background-color: #044742;
+  
+}
+
+h1 {
   text-align: center;
-  margin: 10px;
-  background-color: white;
-  color: black;
+  color: #ebebeb;
+  margin-bottom: 20px;
 }
 
-.container {
+.custom-input-container {
+  margin-bottom: 10px;
+}
+
+label {
+  display: block;
+  font-size: 18px;
+  margin-bottom: 5px;
+  color: #ebebeb;
+}
+
+input[type='text'],
+input[type='email'],
+input[type='password'],
+input[type='number'],
+input[type='date'],
+textarea,
+select {
+  width: 50%;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #000000;
+}
+
+
+
+.custom-check-container {
+  margin-bottom: 15px;
+}
+
+.custom-checkbox {
   display: flex;
-  gap: 2rem;
-  justify-content: center;
-  margin-top: 1rem;
+  align-items: center;
+  margin-bottom: 5px;
 }
 
-.forms {
-  background-color: brown;
+.custom-checkbox input {
+  margin-right: 10px;
+}
+
+.custom-bio-container {
+  margin-bottom: 15px;
+}
+
+textarea {
+  width: 90%;
+  padding: 10px;
+  font-size: 16px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+
+.custom-submit-container {
+  text-align: center;
+}
+
+.custom-submit {
+  width: 200px;
+  padding: 10px;
+  background-color: #175458; 
   color: white;
-  width: 45vw;
-  min-height: 70vh;
-  border-radius: 20px;
-  padding: 20px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 18px;
 }
 
-.resultado {
-  background-color: black;
+.custom-submit:hover {
+  background-color: #17b9a4;
+}
+
+.usuario > p{
   color: white;
-  width: 45vw;
-  min-height: 70vh;
-  border-radius: 20px;
-  padding: 20px;
+  padding: 3px;
+  margin-bottom: 10px;
+  
 }
 
-.row {
-  margin: 5px;
-}
 </style>
